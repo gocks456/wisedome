fcbb35a22fc4867ac4cb8f45907eefe4a3343c41
Yang Haechan <ubuntu@ip-172-31-46-11.ap-northeast-2.compute.internal>
2020-07-24 16:58:03 +0900

    2020.07.24. EC2
    Former-commit-id: ff462523f7b5661f9e2264d3199207801c8be9d8

--- project_order/POS_test1/create.sh
+++
@@ -1,6 +0,0 @@
-# project.json
-pbs --server http://220.68.54.36:5000 --api-key 3e9168a4-4879-46be-ac9c-65e022af2df0 create-project
-# pos_task.csv
-pbs --server http://220.68.54.36:5000 --api-key 3e9168a4-4879-46be-ac9c-65e022af2df0 add-tasks --tasks-file TASKs.csv
-# template.html, results.html, long_description.md
-pbs --server http://220.68.54.36:5000 --api-key 3e9168a4-4879-46be-ac9c-65e022af2df0 update-project
