954c5c38d6d97f952a54508e42abbc60500d3375
Haechan <gocks456@naver.com>
2021-03-02 14:25:43 +0900

    2021.03.02.urd_1
    Former-commit-id: 442091059d8574473ff137e9da02902564639129

--- pybossa/themes/wisedome_theme/static/new_design/js/qnaBoard.js
+++ pybossa/themes/wisedome_theme/static/new_design/js/qnaBoard.js
@@ -13,11 +13,11 @@
     });
     $('#order-listing').each(function() {
       var datatable = $(this);
-      // SEARCH - Add the placeholder for Search and Turn this into in-line form control
+   
       var search_input = datatable.closest('.dataTables_wrapper').find('div[id$=_filter] input');
       search_input.attr('placeholder', '검색');
       search_input.removeClass('form-control-sm');
-      // LENGTH - Inline-Form control
+      
       var length_sel = datatable.closest('.dataTables_wrapper').find('div[id$=_length] select');
       length_sel.removeClass('form-control-sm');
     });
